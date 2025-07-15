1335e3548f3467274607a43e3a1acddb0787b89b
Zingo Lam <zingolam@gmail.com>
2025-07-15 16:08:13 +0800

    undo data folder directory

--- glassnode_data_service.py
+++ glassnode_data_service.py
@@ -17,9 +17,6 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_colwidth', None)
 
-data_folder = 'data'
-os.makedirs(data_folder, exist_ok=True)
-
 
 class GlassnodeDataService:
 
